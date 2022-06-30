61678b7ebb757a432259bba4e605ac5fd29f75f2
Vanille Debray <vanille.paitier-debray@mail.mcgill.ca>
2022-06-30 15:19:38 -0400

    new text and fade ins

--- js/scripts.js
+++ js/scripts.js
@@ -37,6 +37,6 @@
 
     // Activate scrollspy to add active class to navbar items on scroll
     $("body").scrollspy({
-        target: "#sideNav",
+        target: "#topNav",
     });
 })(jQuery); // End of use strict
